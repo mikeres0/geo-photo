304f090ae38f5e33055c71189654ea5cf22040e9
jxsoncenxj <jasoncenaj@gmail.com>
2023-09-06 12:43:12 +0100

    fixed piexif unsigned value handling

--- test2.py
+++ test2.py
@@ -5,13 +5,28 @@ def add_geolocation(image_path, latitude, longitude):
 
     # Convert latitude and longitude to degrees, minutes, seconds format
     def deg_to_dms(deg):
+        deg = abs(deg)  # Ensure the degree is always positive
         d = int(deg)
         m = int((deg - d) * 60)
-        s = int(((deg - d) * 60 - m) * 60)
-        return ((d, 1), (m, 1), (s, 1))
+        s = ((deg - d) * 60 - m) * 60
+
+        # Handle the case when seconds are close to 60, due to float inaccuracies
+        if s >= 59.999:
+            s = 0
+            m += 1
+
+        # Handle the case when minutes become 60
+        if m == 60:
+            m = 0
+            d += 1
+
+        return ((d, 1), (m, 1), (int(s * 100), 100))
 
     lat_dms = deg_to_dms(latitude)
     lon_dms = deg_to_dms(longitude)
+
+    print("Latitude DMS:", lat_dms)  # Debugging Line
+    print("Longitude DMS:", lon_dms) # Debugging Line
 
     exif_dict["GPS"][piexif.GPSIFD.GPSLatitude] = lat_dms
     exif_dict["GPS"][piexif.GPSIFD.GPSLongitude] = lon_dms
